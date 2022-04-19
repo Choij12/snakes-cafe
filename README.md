3d4d31edde1ba71cd7e1bf8ce94d3794e420c2ab
Jacob Choi <jacob.j.choi@gmail.com>
2022-04-18 17:58:58 -0700

    added menu

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# snakes-cafe
+# Lab - Class 01
+
+## Project: Snakes Cafe
+
+### Author: 
+Jacob Choi
+
+### Tests: 
+Testing was not required for this lab
---
+++ snakes_cafe.py
@@ -0,0 +1,66 @@
+print('**************************************')
+print('**   Welcome to the Snakes Cafe!    **')
+print('**    Please See Our Menu Below     **')
+print('** To quit at any time, type "Quit" **')
+print('**************************************')
+
+appetizers = ['Wings', 'Cookies', 'Spring Rolls']
+entrees = ['Salmon', 'Steak', 'Meat Tornado', 'A Literal Garden']
+desserts = ['Ice Cream', 'Cake', 'Pie']
+drinks = ['Coffee', 'Tea', 'Unicorn Tears']
+
+print('')
+print(""" Appetizers """)
+print("----------")
+
+for appetizer in appetizers:
+    print(appetizer)
+
+print('')
+print(""" Entrees """)
+print("----------")
+
+for entree in entrees:
+    print(entree)
+
+print('')
+print(""" Desserts """)
+print("----------")
+
+for dessert in desserts:
+    print(dessert)
+
+print('')
+print(""" Drinks """)
+print("----------")
+
+for drink in drinks:
+    print(drink)
+
+items = {}
+
+print('***********************************')
+print('** What would you like to order? **')
+print('***********************************')
+while True:
+    order = input('> ').capitalize()
+    plural_order = 'orders'
+    singular_order = 'order'
+    have = 'have'
+    has = 'has'
+    if order == 'Quit':
+        break
+    elif order not in desserts + appetizers + drinks + entrees:
+        print("Sorry that's not available from our menu")
+        continue
+    elif order not in items:
+        items[order] = 0
+
+    items[order] += 1
+    if items[order] > 1:
+        plural_order = plural_order
+        have = have
+    else:
+        plural_order = singular_order
+        have = has
+    print(f"** {items[order]} {plural_order} of {order} {have} been added to your meal**")
